bebdab90204a3a2e97e8e1fd04437a3e0a5d0822
Ash Laidlaw <ashlaidlaw@Ashs-MacBook-Pro.local>
2017-01-12 22:09:41 -0800

    Consolidated the index.html and quiz.html pages and removed the quiz page. All content now stays on the index.html page

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,24 @@
 $(function() {
-  $("#name_age").submit(function(event) {
+  $("#get_started").click(function() { // When button on index.html is clicked, it shows the next "page" //
+    $("#index_content").hide();
+    $(".jumbotron").text("Here we go!");
+    $("#quiz_intro").show();
+    $("#name_age").show();
+  });
+
+  $("#name_age").submit(function(event) { // This is the button to initiate the quiz section //
     var name = $("input#user_name").val();
 
-    if (!name) {
+    if (!name) { // Forces user to enter something //
       alert("Please enter your name.")
     } else {
-      $("header h1").text("Welcome, " + name + "!");
-      $("header h3").hide();
+      $("h1").text("Welcome, " + name + "!");
     }
+
+    $("h3").text("Thanks! Now, we're going to ask some questions to get a better idea of what you like.")
+    $("#name_age").hide();
+    $("#questions").show();
+
 
     event.preventDefault();
   });
