f352d104ca8e8328b7d75283a1f269cd5d44641f
Ash Laidlaw <ashlaidlaw@Ashs-MacBook-Pro.local>
2017-01-13 12:31:11 -0800

    Add new form section which will branch depending on user's coding experience

--- js/scripts.js
+++ js/scripts.js
@@ -8,9 +8,21 @@ $(function() {
 
   $("#name_age").submit(function(event) { // This is the button to initiate the survey section //
     var name = $("input#user_name").val();
+    var experience = $("#experience").val();
 
     if (!name) { // Forces user to enter something in field //
       alert("Please enter your name.")
+    } else if (experience === "veteran") {
+      $("h1").text("Hey, " + name + "!");
+      $("#name_age").hide()
+      $("#veteran").show()
+      $("#quiz_intro").hide()
+    } else if (experience === "some" || experience === "none") {
+      $("h1").text("Welcome, " + name + "!");
+      $("#name_age").hide()
+      $("#some").show()
+      $("#interests").show()
+      $("#quiz_intro").hide()
     } else {
       $("h1").text("Welcome, " + name + "!");
       $("#name_age").hide()
@@ -27,21 +39,23 @@ $(function() {
     var app_or_web = $("input:radio[name=app_or_web]:checked").val();
     var mobile_or_business = $("input:radio[name=mobile_or_business]:checked").val();
 
-    if (!interests || !app_or_web || !mobile_or_business) {
+    if (!interests || !app_or_web || !mobile_or_business) { // Forces user to enter something in fields //
       alert("Please make sure to fill out all of the answers.")
     } else if ((interests === "both" || interests === "front") && app_or_web === "web" && mobile_or_business === "flexible") {
+      $("#some").hide()
       $("#interests").hide()
       $("#after_name_input_text").hide()
       $("#front_end_recommend").show()
     } else if (interests === "both" && app_or_web === "app" && mobile_or_business === "large_scale") {
+      $("#some").hide()
       $("#interests").hide()
       $("#after_name_input_text").hide()
       $("#java_android_recommend").show();
     } else if (interests === "both" && app_or_web === "app" && mobile_or_business === "flexible") {
-       $("#interests").hide()
-       $("h3").text("It sounds like you're interested in small-scale application development. We recommend either our Java and Android track, or our Ruby and Rails track!")
+      $("#some").hide()
+      $("#interests").hide()
+      $("h3").text("It sounds like you're interested in small-scale application development. We recommend either our Java and Android track, or our Ruby and Rails track!")
     }
-
 
     event.preventDefault();
   });
